2bfa4f0d580f38f244c72e4e79fa4cef52c03c66
yashpurkar <yashpurkar7079@gmail.com>
2022-12-04 15:03:49 +0530

    got reference

---
+++ index.js
@@ -0,0 +1,5 @@
+const buyingPrice = document.querySelector("#buying_price");
+const stockQuantity = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#current_price");
+const showBtn = documetn.querySelector("#submit_btn");
+const outputBox = document.querySelector(".output_box");
