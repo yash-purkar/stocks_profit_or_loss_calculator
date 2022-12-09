b51f85083d7ace0c4869502e5cb996964a3fc3ad
yashpurkar <yashpurkar7079@gmail.com>
2022-12-09 14:32:48 +0530

    updated

--- index.js
+++ index.js
@@ -27,25 +27,40 @@ function handlwShow() {
 
 
 function calculateProfitAndLoss(buying, quantity, current) {
+
   if (buying > current) { //loss
-    let loss = (buying - current) * quantity;
+    if (buying <= 0 || current <= 0) {
+      outputBox.innerText = "All values should be greater than 0";
+      outputBox.style.color = "red";
+    }
+    else {
 
-    let lossPercent = (loss / (buying * quantity)) * 100;
+      let loss = (buying - current) * quantity;
 
-    showOutput(`Ohhh!! Your Loss is <b>${loss.toFixed(2)}</b> 😧 and loss Percentage is <b>${lossPercent.toFixed(2)}%</b> 😔`)
-    outputBox.style.color = "red";
-    document.querySelector("#main").style.borderColor = "red"
-    document.querySelector("#heading").style.color = "red"
+      let lossPercent = (loss / (buying * quantity)) * 100;
+
+      showOutput(`Ohhh!! Your Loss is <b>${loss.toFixed(2)}</b> 😧 and loss Percentage is <b>${lossPercent.toFixed(2)}%</b> 😔`)
+      outputBox.style.color = "red";
+      document.querySelector("#main").style.borderColor = "red"
+      document.querySelector("#heading").style.color = "red"
+    }
   }
   else if (current > buying) { //profit
-    let profit = (current - buying) * quantity;
+    if (current <= 0 || buying <= 0) {
+      outputBox.innerText = "All values should be greater than 0";
+      outputBox.style.color = "red";
+    }
+    else {
+      let profit = (current - buying) * quantity;
 
-    let profitPercent = (profit / (buying * quantity)) * 100;
+      let profitPercent = (profit / (buying * quantity)) * 100;
 
-    showOutput(`Yay!! Your Profit is <b>${profit.toFixed(2)}</b> 💥 and profit Percentage is <b>${profitPercent.toFixed(2)}% 🚀</b>`)
-    outputBox.style.color = "#31ff31";
-    document.querySelector("#main").style.borderColor = "#31ff31"
-    document.querySelector("#heading").style.color = "#31ff31"
+      showOutput(`Yay!! Your Profit is <b>${profit.toFixed(2)}</b> 💥 and profit Percentage is <b>${profitPercent.toFixed(2)}% 🚀</b>`)
+      outputBox.style.color = "#31ff31";
+      document.querySelector("#main").style.borderColor = "#31ff31"
+      document.querySelector("#heading").style.color = "#31ff31"
+    }
+
 
   }
   else { // If they are equal 
